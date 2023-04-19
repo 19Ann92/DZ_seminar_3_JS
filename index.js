939751ea43d165a9cb7b01db4dcde6d55f20d445
Anna Eliseeva <Panterka6868@mail.ru>
2023-04-19 15:21:02 +0300

    task 3

--- index.js
+++ index.js
@@ -13,14 +13,44 @@
 // Создать фукнцию, которая высчитывает 13% от данного числа
 // и выводит в консоль текст "Размер заработной платы за вычетом налогов равен значение"
 
-let wages = +prompt("Введите вашу заработную плату до вычета");
+// let wages = +prompt("Введите вашу заработную плату до вычета");
 
-if (isNaN(wages)) {
-  alert("Значение задано неверно");
+// if (isNaN(wages)) {
+//   alert("Значение задано неверно");
+// }
+
+// const netSalary = (wages) => {
+//   return wages * 0.87;
+// };
+
+// console.log(netSalary(wages));
+
+// Задание 3
+// Пользователь с клавиатуры вводит 3 числа, необходимо создать функцию,
+// которая определяет максимальное значение среди этих чисел
+
+let numOne = +prompt("Введите первое число");
+
+let numTwo = +prompt("Введите второе число");
+
+let numThree = +prompt("Введите третье число");
+
+function maximumNumber(numOne, numTwo, numThree) {
+  let result = 0;
+  if (numOne > numTwo) {
+    if (numOne > numThree) {
+      result = numOne;
+    } else {
+      result = numThree;
+    }
+  } else if (numTwo > numThree) {
+    result = numTwo;
+  } else {
+    result = numThree;
+  }
+  return result;
 }
 
-const netSalary = (wages) => {
-  return wages * 0.87;
-};
+let result = maximumNumber(numOne, numTwo, numThree);
 
-console.log(netSalary(wages));
+console.log(`Максимальное число ${result}`);
